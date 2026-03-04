0cbfbb03f4e756f83bc841f890da206d5cc4032d
asmaamaged2022 <asmaamaged96@gmail.com>
2026-03-04 03:01:55 +0200

    ..

--- js/function.js
+++ js/function.js
@@ -34,3 +34,53 @@ function closePopup() {
     popupEle.classList.remove("active");
   }, 500);
 }
+function showListImages(imagesList) {
+  let imagesInLi = ``;
+  imagesList.forEach(function (productImage) {
+    imagesInLi += ` 
+    <li  onclick="changeSelectedImage(this,'${productImage}')"class="mainBorder rounded-3 me-3 me-md-0 mb-md-3 p-2">
+       <img src="images/products/${productImage}" alt="" class="img-fluid" />
+    </li>`;
+  });
+  return imagesInLi;
+}
+function showPrice(price, discount) {
+  return `
+     <div class="value">
+        <span class="mainColor text-decoration-line-through me-2 mb-0 ${discount == 0 ? "d-none" : ""}">${price}<sup>$</sup></span>
+        <span class="mb-0">${(price * (1 - discount)).toFixed(2)}<sup>$</sup></span>
+      </div>
+  `;
+}
+function showSize(sizeList) {
+  let sizeInLi = ``;
+  sizeList.forEach(function (productSizes, index) {
+    sizeInLi += ` 
+     <li class="mainButton rounded-2 me-2 ${index == 0 ? "active" : ""}">${productSizes}</li>
+      `;
+  });
+  return sizeInLi;
+}
+function showLiListInFeatures(List) {
+  let Lis = ``;
+  List.forEach(function (LiContent, index) {
+    Lis += ` 
+    <li onclick="changeSelectedImage(this,'${LiContent}');changeActive(this)" class="mainButton rounded-circle ${index == List.length - 1 ? "" : "me-3"}  ${index == 0 ? "active" : ""}"></li>
+      `;
+  });
+  return Lis;
+}
+function changeSelectedImage(that, selectedImage) {
+  let currentProduct = that.closest(".product"),
+    currentImage = currentProduct.querySelector(".selectedImg img"),
+    currentSrc = currentImage.src,
+    imgArr = currentSrc.split("/");
+  imgArr[imgArr.length - 1] = selectedImage;
+  currentImage.src = imgArr.join("/");
+}
+function changeActive(that) {
+  let ulEle = that.closest("ul"),
+    currentActive = ulEle.querySelector(".active");
+  currentActive.classList.remove("active");
+  that.classList.add("active");
+}
--- js/index.js
+++ js/index.js
@@ -6,7 +6,9 @@ let nextBtn = document.querySelector("#Carousel button.next"),
   titleImgs = document.querySelectorAll(".title img"),
   navLinks = navEle.querySelectorAll(".nav-link"),
   loading = document.querySelector("#loading"),
-  popupBoxes = document.querySelectorAll(".popup .box");
+  popupBoxes = document.querySelectorAll(".popup .box"),
+  latestContentEle = document.querySelector("#Latest .content"),
+  featuresContentEle = document.querySelector("#Featured .content .row");
 
 nextBtn.addEventListener("click", function () {
   let currentSlide = document.querySelector("#Carousel .my-carousel-item.active"),
@@ -67,4 +69,71 @@ popupBoxes.forEach(function (popupBox) {
   popupBox.addEventListener("click", function (e) {
     e.stopPropagation();
   });
+});
+latest.forEach(function (product) {
+  latestContentEle.innerHTML += `
+   <div class="product bg-light p-3 rounded-3 mainBorder mb-3" data-product-id="${product.id}">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="item">
+            <div class="row">
+              <div class="col-md-2 part1">
+                <ul class="list-unstyled d-md-block d-flex">
+                 ${showListImages(product.images)}
+                </ul>
+              </div>
+              <div class="col-md-10 m-auto part2">
+                <div class="selectedImg">
+                  <img src="images/products/${product.images[0]}" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item">
+            <h2 class="mainColor">${product.name}</h2>
+            <p class="text-secondary">
+           ${product.description}
+            </p>
+             <div class="d-flex price">
+                 <div class="label"><h6 class="mb-0 fw-bolder me-3">Price :</h6></div>
+                    ${showPrice(product.price, product.discount)}
+            </div>
+          
+            <div class="d-flex size mt-3">
+              <div class="label"><h6 class="mb-0 fw-bolder me-2">Size :</h6></div>
+              <div class="value">
+                <ul class="list-unstyled d-flex">
+                 ${showSize(product.sizes)}
+                </ul>
+              </div>
+            </div>
+            <button class="btn mainButton">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>`;
+});
+features.forEach(function (product) {
+  featuresContentEle.innerHTML += `
+   <div class="col-lg-3 col-sm-6 product text-center mb-4">
+      <div class="item rounded-4 bg-light overflow-hidden">
+        <div class="header">
+          <p class="text-light">-16%</p>
+          <div class="selectedImg">
+            <img src="images/products/${product.images[0]}" alt="" class="img-fluid" />
+          </div>
+          <i class="fas fa-search key rounded-circle"></i>
+          <ul class="list-unstyled d-flex justify-content-center mb-0">
+         ${showLiListInFeatures(product.images)} 
+          </ul>
+        </div>
+        <div class="body py-3">
+          <h6>${product.name}</h6>
+          ${showPrice(product.price, product.discount)}
+        </div>
+      </div>
+    </div>
+   `;
 });
